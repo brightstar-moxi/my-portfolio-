8c615ca2f90dacef86eb19bcf106d0fe1005a3a5
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-02-15 16:10:21 +0100

    first commit

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section><h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'> Hello, I'm Brightstar</h1>
+    <p className='text-[#ADB7BE] text-lg lg:text-xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Amet, sit adipisci recusandae earum, id ipsum provident 
+        placeat itaque et accusantium quis quia debitis assumenda, 
+        ex possimus aspernatur. Eveniet, perspiciatis tenetur!</p>
+    </section>
+  )
+}
+
+export default HeroSection
