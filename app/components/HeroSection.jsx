c2edb74e20677776538c89eaa7b61074ae25930e
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-02-16 17:03:42 +0100

    brightstar5

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -12,6 +12,7 @@ const HeroSection = () => {
        <div className='col-span-7 place-self-center text-center sm:text-left'>
        <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
         <span className='text-transparent bg-clip-text bg-gradient-to-r  from-purple-500 to-pink-500'> Hello, I'm {""}</span>
+       <br/>
         <TypeAnimation
   sequence={[
     // Same substring at the start will only be typed once, initially
