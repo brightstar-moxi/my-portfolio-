f845d56619075a6ccaeb03de4c2f2189d9af752f
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-02-21 11:22:22 +0100

    MOXIZ 1

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -21,9 +21,9 @@ const Navbar = () => {
   return (
    <nav>
      <div className='flex flex-wrap items-center justify-between mx-auto p-8'> 
-        <Link href={"/"} className='text-5xl text-white font-semibold '> LOGO</Link>
+        <Link href={"/"} className='text-lg md:text-5xl text-white font-semibold '> LOGO</Link>
         <div className='menu block md:w-auto' id="navbar">
-            <ul>
+            <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
               {
                navLinks.map((link,index) =>(
                 <li key={index}>
