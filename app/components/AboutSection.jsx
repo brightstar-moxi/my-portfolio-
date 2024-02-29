b794443fb028e065b44adbd54c0adc1252006360
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-02-29 09:39:31 +0100

    good1

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+import React, {useTransition, useState} from 'react'
 import Image from "next/image";
 import Image2 from '/public/images/HeroImage.png'
 
 
 const AboutSection = () => {
+    const [tab, SetTab] = useState("skills");
+    const [startTransition, isPending] = useTransition();
+
+
+
+
+
+    const handleTabChange = (id) => {
+        
+    }
   return (
     <section className='text-white'><div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
          <Image 
@@ -14,11 +25,16 @@ const AboutSection = () => {
         />
         <div>
             <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
-            <p>As a software developer, my journey begins with lines of code and algorithms that breathe life into digital landscapes. Proficient in languages such as Python, Java, and JavaScript, 
+            <p className='text-base lg:text-lg'>As a software developer, my journey begins with lines of code and algorithms that breathe life into digital landscapes. Proficient in languages such as Python, Java, and JavaScript, 
                 I embark on the intricate task of solving complex problems and crafting solutions that stand at the intersection of functionality and efficiency.
                  The software development lifecycle becomes my canvas, where I meticulously plan, code, test, and deploy, 
                  ensuring that each line serves a purpose and contributes to the overall success of the project.
                  </p>
+                 <div className='flex flex-row mt-8'>
+                    <span className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500'>Skills</span>
+                    <span>Education</span>
+                    <span>Experience</span>
+                 </div>
         </div>
         </div></section>
   )
