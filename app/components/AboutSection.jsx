f3ed3204b66027e0154ca6254f9f482c6ac161d9
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-02-29 09:55:49 +0100

    good2

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -13,7 +13,10 @@ const AboutSection = () => {
 
 
     const handleTabChange = (id) => {
-        
+        startTransition(() =>{
+            SetTab(id);
+        });
+
     }
   return (
     <section className='text-white'><div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
---
+++ app/components/TabButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TabButton = ({active, selectTab, children}) => {
+  return (
+    <button 
+    className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500'>
+        {children}
+        </button>
+  )
+}
+
+export default TabButton
