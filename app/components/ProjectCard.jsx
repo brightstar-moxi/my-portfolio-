4549c46661ac170f0769849799a225f0957094b0
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-06 15:33:38 +0100

    today update 3

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ProjectCard = ({imgUrl, title, description}) => {
   return (
-    <div><div style={{background: `url(${imgUrl})`, backgroundSize:"cover"}}></div></div>
+    <div><div className='h-52 md:h-72'
+     style={{background: `url(${imgUrl})`, backgroundSize:"cover"}}></div>
+     <div className='text-white'>
+        <h5>{title}</h5>
+        <p>{description}</p>
+        </div>
+     </div>
   )
 }
 
--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
+
+const projectData = [
+    {
+        id: 1,
+        title: "React animated Portfolio",
+        description: "Project 1 description",
+        image: "/image/project/1.png"
+    }, {
+        id: 2,
+        title: "React animated Portfolio",
+        description: "Project 2 description",
+        image: "/image/project/1.png"
+    }, {
+        id: 3,
+        title: "React animated Portfolio",
+        description: "Project 3 description",
+        image: "/image/project/1.png"
+    }, {
+        id: 4,
+        title: "React animated Portfolio",
+        description: "Project 4 description",
+        image: "/image/project/1.png"
+    }
+]
 
 const ProjectSection = () => {
-  return (
-    <><h2>My Project</h2><div></div></>
-  )
+
+    return (
+        <><h2>My Project</h2>
+            <div>{projectData.map((project) =>
+                <ProjectCard
+                    key={project.id}
+                    title={project.title}
+                    description={project.description}
+                    imgUrl={project.image}
+                    tags={project}
+                />)}</div></>
+    )
 }
 
 export default ProjectSection
