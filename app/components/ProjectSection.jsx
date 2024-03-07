761b8328937b2bb65d83e80c983a494720293260
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-07 18:04:12 +0100

    projectPage Updated3

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -9,22 +9,33 @@ const projectData = [
         title: "React animated Portfolio",
         description: "Project 1 description",
         image: "/images/project/1.png",
-        tag: ["All","Web"]
+        tag: ["All","Web"],
+        gitUrl:"/",
+        previewUrl:"/"
     }, {
         id: 2,
         title: "React animated Portfolio",
         description: "Project 2 description",
-        image: "/images/project/1.png"
+        image: "/images/project/1.png",
+        tag: ["All","Web"],
+        gitUrl:"/",
+        previewUrl:"/"
     }, {
         id: 3,
         title: "React animated Portfolio",
         description: "Project 3 description",
-        image: "/images/project/1.png"
+        image: "/images/project/1.png",
+        tag: ["All","Web"],
+        gitUrl:"/",
+        previewUrl:"/"
     }, {
         id: 4,
         title: "React animated Portfolio",
         description: "Project 4 description",
-        image: "/images/project/1.png"
+        image: "/images/project/1.png",
+        tag: ["All","Web"],
+        gitUrl:"/",
+        previewUrl:"/"
     }
 ]
 
@@ -32,13 +43,21 @@ const ProjectSection = () => {
 
     return (
         <><h2 className='text-center text-4xl font-bold text-white mt-4'>My Projects</h2>
+            <div  className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+                <button className='rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer'>All</button>
+                <button className='rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer'>Web</button>
+                <button className='rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer'>Design</button>
+            </div>
             <div className='grid md:grid-cols-3 gap-8 md:gap-12'>{projectData.map((project) =>
+                
                 <ProjectCard
                     key={project.id}
                     title={project.title}
                     description={project.description}
                     imgUrl={project.image}
                     tags={project}
+                    gitUrl={project.gitUrl}
+                    previewUrl={project.previewUrl}
                 />)}</div></>
     )
 }
