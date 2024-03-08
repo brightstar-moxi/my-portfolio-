dcdbc97a02c07df5d877eaf269dfc3bf80c7d657
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-08 11:14:44 +0100

    projectPage Updated7

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React from 'react';
+import GithubIcon from "../../public/icons8-github.svg";
+import LinkedinIcon from "../../public/icons8-linkedin.svg";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const EmailSection = () => {
-  return<section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
-    <div><h5 className='text-xl font-bold text-white my-2'>Let `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.s Connect</h5>
-    <p className='text-[#ADB7BE] mb-4 max-w-md'>
-        {" "}
-        I'm currently looking for new opportunities, my inbox is always open.
-        Whether you a question or just want to say hi, I'll try my best to get back to you!
-        </p>
-        <div className='socials flex flex-row gap-2'></div>
+    return <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
+        <div><h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
+            <p className='text-[#ADB7BE] mb-4 max-w-md'>
+                {" "}
+                I&apos;m currently looking for new opportunities, my inbox is always open.
+                Whether you a question or just want to say hi, I&apos;ll try my best to get back to you!
+            </p>
+            <div className='socials flex flex-row gap-2'>
+                <Link href='github.com'>
+                    <Image src={GithubIcon} alt='Github Icon' />
+                </Link>
+                <Link href='linkedin.com'>
+                    <Image src={LinkedinIcon} alt='Linkedin Icon' />
+                </Link>
+            </div>
         </div>
-  </section>
-//   &apos;', &lsquo;`'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.
+        <div>
+            <form className='flex flex-col gap-4'>
+                <label htmlFor='email' className='text-white block mb-2 text-sm font-medium'>Your Email</label>
+                <input type='email' id='email' 
+                className='bg-[&1819E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-'
+                required 
+                placeholder='brightbobo@google.com' 
+                />
+            </form>
+        </div>
+    </section>
+    //   &apos;', &lsquo;`'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.
 }
 
 export default EmailSection
