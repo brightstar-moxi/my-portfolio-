199b80c5a60b9d7ca5884c4477efce4015fd2869
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-09 04:07:01 +0100

    emailPage Updated2

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -5,7 +5,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const EmailSection = () => {
-    return <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
+    return (
+    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
+       <div className=''></div>
         <div><h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {" "}
@@ -41,10 +43,33 @@ const EmailSection = () => {
                     />
                 </div>
 
+                <div className='mb-6'>
+                <label htmlFor='message' 
+                className='text-white block mb-2 text-sm font-medium'>
+                   Message
+                    </label>
+                    <textarea name='message' id='message'
+                     className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                     placeholder='Let&apos;s talk about...'
+                    //  value="Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    //  Amet, sit adipisci recusandae earum, id ipsum provident 
+                    //  placeat itaque et accusantium quis quia debitis assumenda, 
+                    //  ex possimus aspernatur. Eveniet, perspiciatis tenetur!
+                    //  Lorem, ipsum dolor sit amet consectetur adipisicing
+                    //   elit. Officia cupiditate et officiis accusamus earum 
+                    //   sequi corporis, totam animi magni ad sit, voluptates "
+                    />
+                </div>
+                <button
+                type='submit'
+                className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>
+                    Send Message
+                </button>
+
             </form>
         </div>
     </section>
     //   &apos;', &lsquo;`'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.
-}
+)}
 
 export default EmailSection
