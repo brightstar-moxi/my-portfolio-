321238718313cb1d5bf10398fa65663f9f5ea162
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-14 12:20:27 +0100

    email updated 3

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -5,72 +5,101 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const EmailSection = () => {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.email.value,
+            message: e.target.email.value
+        }
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "/api/Send";
+
+        //form the request for sending data to the server.
+        const options ={
+            //The method is POST because we are sending data.
+            method: 'POST',
+            //Tell the server we're sending JSON.
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            //Body of the request is the JSON data we created above.
+            body: JSONdata,
+        }
+        const response = await fetch(endpoint, options);
+        const resData = await response.json();
+        if (resData.status === 'success'){
+            console.log('Message sent,')
+        }
+     }
     return (
-    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
-       <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2  '></div>
-        <div className='z-18'>
-            <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
-            <p className='text-[#ADB7BE] mb-4 max-w-md'>
-                {" "}
-                I&apos;m currently looking for new opportunities, my inbox is always open.
-                Whether you a question or just want to say hi, I&apos;ll try my best to get back to you!
-            </p>
-            <div className='socials flex flex-row gap-2'>
-                <Link href='github.com'>
-                    <Image src={GithubIcon} alt='Github Icon' />
-                </Link>
-                <Link href='linkedin.com'>
-                    <Image src={LinkedinIcon} alt='Linkedin Icon' />
-                </Link>
+        <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
+            <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2  '></div>
+            <div className='z-18'>
+                <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
+                <p className='text-[#ADB7BE] mb-4 max-w-md'>
+                    {" "}
+                    I&apos;m currently looking for new opportunities, my inbox is always open.
+                    Whether you a question or just want to say hi, I&apos;ll try my best to get back to you!
+                </p>
+                <div className='socials flex flex-row gap-2'>
+                    <Link href='github.com'>
+                        <Image src={GithubIcon} alt='Github Icon' />
+                    </Link>
+                    <Link href='linkedin.com'>
+                        <Image src={LinkedinIcon} alt='Linkedin Icon' />
+                    </Link>
+                </div>
             </div>
-        </div>
-        <div>
-            <form className='flex flex-col'>
-                <div className='mb-6'>
-                    <label htmlFor='email' className='text-white block mb-2 text-sm font-medium'>Your Email</label>
-                    <input type='email' id='email'
-                        className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                        required
-                        placeholder='brightbobo@google.com'
-                    />
-                </div>
+            <div>
+                <form className='flex flex-col'>
+                    <div className='mb-6'>
+                        <label htmlFor='email' className='text-white block mb-2 text-sm font-medium'>Your Email</label>
+                        <input type='email' id='email'
+                            className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                            required
+                            placeholder='brightbobo@google.com'
+                        />
+                    </div>
 
-                <div className='mb-6'>
-                    <label htmlFor='subject' className='text-white block mb-2 text-sm font-medium'>Subject</label>
-                    <input type='text' id='subject'
-                        className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                        required
-                        placeholder='just say hi'
-                    />
-                </div>
+                    <div className='mb-6'>
+                        <label htmlFor='subject' className='text-white block mb-2 text-sm font-medium'>Subject</label>
+                        <input type='text' id='subject'
+                            className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                            required
+                            placeholder='just say hi'
+                        />
+                    </div>
 
-                <div className='mb-6'>
-                <label htmlFor='message' 
-                className='text-white block mb-2 text-sm font-medium'>
-                   Message
-                    </label>
-                    <textarea name='message' id='message'
-                     className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                     placeholder='Let&apos;s talk about...'
-                    //  value="Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    //  Amet, sit adipisci recusandae earum, id ipsum provident 
-                    //  placeat itaque et accusantium quis quia debitis assumenda, 
-                    //  ex possimus aspernatur. Eveniet, perspiciatis tenetur!
-                    //  Lorem, ipsum dolor sit amet consectetur adipisicing
-                    //   elit. Officia cupiditate et officiis accusamus earum 
-                    //   sequi corporis, totam animi magni ad sit, voluptates "
-                    />
-                </div>
-                <button
-                type='submit'
-                className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>
-                    Send Message
-                </button>
+                    <div className='mb-6'>
+                        <label htmlFor='message'
+                            className='text-white block mb-2 text-sm font-medium'>
+                            Message
+                        </label>
+                        <textarea name='message' id='message'
+                            className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                            placeholder='Let&apos;s talk about...'
+                        //  value="Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        //  Amet, sit adipisci recusandae earum, id ipsum provident 
+                        //  placeat itaque et accusantium quis quia debitis assumenda, 
+                        //  ex possimus aspernatur. Eveniet, perspiciatis tenetur!
+                        //  Lorem, ipsum dolor sit amet consectetur adipisicing
+                        //   elit. Officia cupiditate et officiis accusamus earum 
+                        //   sequi corporis, totam animi magni ad sit, voluptates "
+                        />
+                    </div>
+                    <button
+                        type='submit'
+                        className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>
+                        Send Message
+                    </button>
 
-            </form>
-        </div>
-    </section>
-    //   &apos;', &lsquo;`'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.
-)}
+                </form>
+            </div>
+        </section>
+        //   &apos;', &lsquo;`'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.
+    )
+}
 
 export default EmailSection
