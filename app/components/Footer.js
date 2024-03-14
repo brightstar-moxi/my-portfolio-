fd3105a99274cec0601ea4fe9da79ebd270dec1d
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-14 11:20:15 +0100

    email updated 1

---
+++ app/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer><div><span>LOGO</span><p>All right reserved</p></div></footer> 
+  )
+}
+
+export default Footer
--- app/page.js
+++ app/page.js
@@ -4,18 +4,21 @@ import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
 import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] "> 
 
 <Navbar/>
-<container className= 'container mt-24 mx-auto px-12 py-4'>
+<div className= 'container mt-24 mx-auto px-12 py-4'>
 <HeroSection/>
 <AboutSection/>
 <ProjectSection/>
 <EmailSection/>
-</container>
+
+</div>
+<Footer/>
      </main>
   )
 }
