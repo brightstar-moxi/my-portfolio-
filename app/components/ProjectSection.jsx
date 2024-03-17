1be4868c2e7170b8e8fb34d023a980eb501cab03
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-17 03:56:01 +0100

    animation updated2

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useRef } from 'react';
 import ProjectCard from './ProjectCard';
 import ProjectTag from './ProjectTag';
-import {motion, useInView} from "framer-motion"
+import { motion, useInView } from "framer-motion"
 
 const projectData = [
     {
@@ -47,38 +47,57 @@ const projectData = [
 
 const ProjectSection = () => {
     const [tag, setTag] = useState("All");
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
 
     const handleTagChange = (newTag) => {
         setTag(newTag);
     };
-  const filteredProject = projectData.filter((project) => project.tag.includes(tag)
-  )
+    const filteredProject = projectData.filter((project) => project.tag.includes(tag)
+    );
+
+    const cardVariants = {
+
+        initial: { y: 50, opacity: 0 },
+        animate: { y: 0, opacity: 1 }
+    }
     return (
-        <><h2 className='text-center text-4xl font-bold text-white mt-4'>My Projects</h2>
+        <section >
+            <h2 className='text-center text-4xl font-bold text-white mt-4'>My Projects</h2>
             <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
                 <ProjectTag onClick={handleTagChange}
-                   name="All"
+                    name="All"
                     isSelected={tag === "All"} />
 
-                     <ProjectTag onClick={handleTagChange}
-                   name="Web"
+                <ProjectTag onClick={handleTagChange}
+                    name="Web"
                     isSelected={tag === "Web"} />
-                      <ProjectTag onClick={handleTagChange}
-                   name="Design"
+                <ProjectTag onClick={handleTagChange}
+                    name="Design"
                     isSelected={tag === "Design"} />
             </div>
-            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-                {filteredProject.map((project) =>
+            <ul ref={ref} className='grid md:grid-cols-3 gap-8 md:gap-12'>
+                {filteredProject.map((project, index) => (
+                    <motion.li key={index}
+                        variants={cardVariants}
+                        initial="initial" animate={isInView ?
+                            "animate" : "initial"}
+                            transition={{duration: 0.3, delay: index = 0.4}}
+                            >
 
-                    <ProjectCard
-                        key={project.id}
-                        title={project.title}
-                        description={project.description}
-                        imgUrl={project.image}
-                        // tags={project}
-                        gitUrl={project.gitUrl}
-                        previewUrl={project.previewUrl}
-                    />)}</div></>
+                        <ProjectCard
+                            key={project.id}
+                            title={project.title}
+                            description={project.description}
+                            imgUrl={project.image}
+                            // tags={project}
+                            gitUrl={project.gitUrl}
+                            previewUrl={project.previewUrl}
+                        />
+                    </motion.li>
+                ))}
+            </ul>
+        </section>
     )
 }
 
--- app/components/TabButton.jsx
+++ app/components/TabButton.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {motion} from "framer-motion";
+
+const variants={
+  default: {width:0},
+  active: {width: "calc(100% - 0.75)"}
+}
 
 const TabButton = ({active, selectTab, children}) => {
     const buttonClasses = active ? 'text-white border-b border-primary-500' : "text-[#ADB7BE] "
   return (
     <button onClick={selectTab}>
         <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}> {children}</p>
+        <motion.div animate={active ? "active": "default"} variants={variants}
+        className='h-1 bg-primary-500 mt-2 mr-3'
+        ></motion.div>
         {/* mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500 */}
        
         </button>
