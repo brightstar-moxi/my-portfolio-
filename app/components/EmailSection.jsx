662a7053e8ae213d41dd7b1281db37df347512f3
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-18 10:35:31 +0100

    updated3

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -46,7 +46,7 @@ const EmailSection = () => {
                 <p className='text-[#ADB7BE] mb-4 max-w-md'>
                     {" "}
                     I&apos;m currently looking for new opportunities, my inbox is always open.
-                    Whether you a question or just want to say hi, I&apos;ll try my best to get back to you!
+                    Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
                 </p>
                 <div className='socials flex flex-row gap-2'>
                     <Link href='github.com'>
