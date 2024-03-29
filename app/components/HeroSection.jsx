8c99a6b99b3665a787a320df1645a7140ab0304a
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-03-29 13:08:27 +0100

    cv download update

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,13 +1,25 @@
 "use client";
-import React from 'react';
+import {React,useState } from 'react';
 import Image from "next/image";
 import Image1 from '/public/images/Capture-removebg-preview.png';
 import { TypeAnimation } from 'react-type-animation';
 import {motion} from "framer-motion";
 import Link from 'next/link';
+import { useRouter } from "next/navigation";
+
 
 
 const HeroSection = () => {
+  // const router = useRouter();
+  // const goToNewPage = () => {
+  //   router.push('/cv');
+  // }
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopover = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <section className='lg:py-16'>
        <div className='grid grid-cols-1 sm:grid-cols-12'>
@@ -47,14 +59,20 @@ via-purple-500 */}
             >
               Hire Me
             </Link>
-            <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            <button
+             onClick={togglePopover}
+              className="relative px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
-              </span>
-            </Link>
+              </span >
+               {/* Popover content */}
+      {isOpen && (
+        <div className="absolute top-full left-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-10">
+          <div className="p-2 "><button className=" px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-primary-500 hover:bg-slate-800 text-white mt-3">Click here to Download</button></div>
+        </div>
+      )}
+            </button>
        </div>
        </motion.div>
 
---
+++ app/cv/cv.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Cv  () {
+  return (
+    <div >cvvvtin bhfin lor</div>
+  )
+}
+
