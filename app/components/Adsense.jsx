5ffadb994b7163f80a5f21b9719481c2a9663b4b
brightstar-moxi <brightstarmoxiz2@gmail.com>
2024-04-11 13:13:35 +0100

    update

--- app/components/Adsense.jsx
+++ app/components/Adsense.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const AdSense = ({ pId }) => {
   return (
-    <script async src= {`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client==ca-pub-${pId}`}
-    crossorigin="anonymous"></script>
+    <script async src= {`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+    crossorigin="anonymous"  strategy='afterInteractive'></script>
     
     
   );
